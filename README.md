2a43097ca41e6ebf5a75b021f80b7ef82e835fc9
shensq <shensq@umich.edu>
2024-02-08 14:50:29 -0500

    Removed Slurm code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # LLMValueAlignment
 Exploring value alignment for LLMs
+
+
+## Getting Probing Results
+   ```sh
+   scripts/run_probing.sh
+   ```
+
+
+## Calculating Alignment Scores
+   ```sh
+   scripts/compute_alignment.sh
+   ```
+
+## Running Analysis Experiments
+   ```sh
+   scripts/run_analysis.sh
+   ```
--- scripts/run_probing.sh
+++ scripts/run_probing.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-#SBATCH --partition=spgpu
-#SBATCH --gpus=1
-#SBATCH --cpus-per-gpu=8
-#SBATCH --mem-per-gpu=128GB
-#SBATCH --account=mihalcea0
-#SBATCH --time=00-08:00:00 
-
-# set up job
-module load python/3.9.12 cuda
-pushd /home/mehars/LLMValueAlignment
-source ./myenv/bin/activate
-
 # run job
 # python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/Llama-2 --model_name meta-llama/Llama-2-7b-chat-hf --result_dir results/Llama-2-7b-chat-hf
 # python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/Llama-2 --model_name meta-llama/Llama-2-13b-chat-hf --result_dir results/Llama-2-13b-chat-hf
 # python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/without_system_prompt --model_name bigscience/bloomz-3b --result_dir results/bloomz-3b
 # python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/without_system_prompt --model_name bigscience/bloomz-7b1 --result_dir results/bloomz-7b1
 python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/without_system_prompt --model_name tiiuae/falcon-7b-instruct --result_dir results/falcon-7b-instruct
-python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/without_system_prompt --model_name tiiuae/falcon-40b-instruct --result_dir tiiuae/falcon-40b-instruct
-python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/Mistral --model_name mistralai/Mistral-7B-Instruct-v0.2 --result_dir results/Mistral-7B-Instruct-v0.2
+#python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/without_system_prompt --model_name tiiuae/falcon-40b-instruct --result_dir tiiuae/falcon-40b-instruct
+#python src/probe_model/probe_all_prompts.py --dataset_fp wvs_data/questionaire/clean_wvs.csv --prompt_dir src/prompt_templates/Mistral --model_name mistralai/Mistral-7B-Instruct-v0.2 --result_dir results/Mistral-7B-Instruct-v0.2
