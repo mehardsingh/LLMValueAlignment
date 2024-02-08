e4fd34eb8682d795394dbbaa81dc5b84949aec6c
shensq <shensq@umich.edu>
2024-02-08 15:45:34 -0500

    Computer_spearman now get the spearman for the average by default. Reindex the prompts based on format

--- src/analysis/compute_spearman.py
+++ src/analysis/compute_spearman.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from scipy.stats import spearmanr
 import matplotlib.pyplot as plt
@@ -12,8 +13,12 @@ def save_spearman(in_align_fp, out_fp):
     num_prompts = alignment_df['Prompt'].nunique()
     spearman_heatmap = np.zeros((num_prompts, num_prompts))
 
+    # A temporary fix
+    prompt_idx = {0:1, 1:3, 2:5, 3:7, 4:9, 5:2, 6:4, 7:6, 8:8, 9:10, 10:11, 11:12, 12:13, 13:14, 14:15}
+
     for i in range(num_prompts):
-        p1_df =  alignment_df[alignment_df["Prompt"] == f"pt{i+1}"]
+        # p1_df =  alignment_df[alignment_df["Prompt"] == f"pt{i+1}"]
+        p1_df = alignment_df[alignment_df["Prompt"] == f"pt{prompt_idx[i]}"]
         p1_df = p1_df[["Country", "Alignment"]]
         p1_df = p1_df.groupby('Country')['Alignment'].mean().reset_index()
         p1_df.columns = ['Country', 'Alignment']
@@ -43,11 +48,43 @@ def save_spearman(in_align_fp, out_fp):
 
     plt.savefig(out_fp)
 
+
+def write_average_alignment(folder_path):
+
+    # Get all CSV files in the folder
+    csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+
+    # Iterate over each CSV file
+    dfs = []
+    for file in csv_files:
+        file_path = os.path.join(folder_path, file)
+
+        # Read the CSV file into a DataFrame
+        df = pd.read_csv(file_path)
+
+        # Append the DataFrame to the list
+        dfs.append(df)
+
+    # Concatenate all DataFrames into a single DataFrame
+    concatenated_df = pd.concat(dfs)
+
+    # Group the rows by the first three columns and calculate the average for each group
+    grouped_df = concatenated_df.groupby(['Country', 'Prompt', 'Section']).mean()
+
+    grouped_df.to_csv(os.path.join(folder_path, 'alignment_all_models.csv'), index=True, header=True)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--in_align_fp", type=str)
     parser.add_argument("--out_fp", type=str)
     args = parser.parse_args()
 
-    save_spearman(args.in_align_fp, args.out_fp)
+    if not args.in_align_fp and not args.out_fp:
+        folder_path = "analysis_metrics"
+        write_average_alignment(os.path.join(folder_path, "alignment"))
+        save_spearman(os.path.join(folder_path, "alignment", "alignment_all_models.csv"),
+                      os.path.join(folder_path, "spearman_plots" ,"spearman_all_models.png"))
+    else:
+        save_spearman(args.in_align_fp, args.out_fp)
 
