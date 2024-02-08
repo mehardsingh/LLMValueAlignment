b8f9dc65af51c03c1c77c490f9b76fdbfe74ef1f
shensq <shensq@umich.edu>
2024-02-08 16:05:54 -0500

    Load model with accelerate

--- src/probe_model/probe_utils.py
+++ src/probe_model/probe_utils.py
@@ -39,7 +39,7 @@ def get_prompt_inputs(dataset_fp, prompt_template_fp):
 def query_model(model, tokenizer, prompt_inputs, get_probs):
     logging.info("Inside query_model()")
 
-    generator = pipeline("text-generation", model=model, tokenizer=tokenizer, device="auto")
+    generator = pipeline("text-generation", model=model, tokenizer=tokenizer, )
     all_inputs_responses = generator([p["prompt"] for p in prompt_inputs], max_new_tokens=10)
     all_inputs_responses = [r[0]["generated_text"] for r in all_inputs_responses]
 
@@ -60,7 +60,7 @@ def query_model(model, tokenizer, prompt_inputs, get_probs):
         prompt = prompt_input["prompt"]
         num_choices = prompt_input["num_choices"]
 
-        input = tokenizer(prompt, return_tensors="pt").to(device)
+        input = tokenizer(prompt, return_tensors="pt").to("cuda")
 
         with torch.no_grad():
             outputs = model(**input)
